3630dc7f7c853a91629748e042f856bedf7424ca
김 미성 <rlaaltjd46@jinhak.com>
2018-07-03 18:23:54 +0900

    1

--- 02.express-tutorial/main.js
+++ 02.express-tutorial/main.js
@@ -12,7 +12,7 @@ var bodyParser = require("body-parser");
 
 app.use(morgan("dev")); //터미널에 logging정보 뜸
 app.use(bodyParser.json()); //json 형태의 body를 읽을 수 있게 해줘라고 설정
-app.use("/", express.static("public")); //'/' 이 경로로 들어왔을 때 public 디렉터토리에 있는 파일에 접근할 수 있게 해줘
+app.use("/", express.static("public")); //express 내의 static 미들웨어 , '/' 이 경로로 들어왔을 때 public 디렉터토리에 있는 파일에 접근할 수 있게 해줘
 // 위에꺼랑 밑에꺼 경로 같은 데 먼저 작성된 순서대로 우선권 가짐
 // app.get("/", function(req, res) {
 //   res.send("hello world");
