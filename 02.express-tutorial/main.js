edb23601168f6675a2c29c700061223016e1770b
김 미성 <rlaaltjd46@jinhak.com>
2018-07-05 16:22:29 +0900

    1

--- 02.express-tutorial/main.js
+++ 02.express-tutorial/main.js
@@ -24,3 +24,13 @@ app.use("/user", user); //'/user'로 요청이 들어오면 user 라우터로 �
 app.listen(3000, function() {
   console.log("Example App listening on port 3000");
 });
+
+app.locals.pretty = true; //jade 코드 예쁘게 해줌
+app.set("view engine", "jade"); // express에 jade템플릿엔진 연결 , view engine은 템플릿 엔진이라는 뜻 약속된 것, 어떤 템플릿 엔진을? -> jade를
+app.set("views", "./views"); // 템플릿이 있는 디렉토리(views : jade파일들)를 express에 알려주는 코드, 이 부분 생략해도 디폴트로 views로 지정되있음
+app.get("/template", function(req, res) {
+  //'/template'라는 경로로 들어온 사용자에게 콜백함수가 실행되면서 temp라고 하는 템플릿 파일을 웹페이지로 렌더링해서 전송한다
+  res.render("temp", { _title: "title", time: Date() }); //첫번째 인자는 템플릿명, 두번째 인자는 jade파일에 정의된 변수들에게 값 전달되는 객체(파라미터 같은?)
+});
+//즉, response로 temp를 렌더하게 되면 express는 내부적으로 정의해놓은 views디렉토리에서 템플릿엔진으로 jade로 정의 해놓았기 때문에
+//jade의 확장자인 temp.jade 찾아서 이 템플릿 파일을 jade의 문법에 따라 해석한 후 사용자에게 response하는 코드
